3a6d796026ec0ab2144e72b6932cd941818b81a1
sachateisseire <sit.teisseire@gmail.com>
2023-01-10 22:55:00 -0300

    duplicado expon e includes

--- nuevoArchivo.js
+++ nuevoArchivo.js
@@ -6,3 +6,13 @@ console.log(cuadrados)
 
 const incluye = cuadrados.includes(16)
 console.log(incluye)
+
+
+const numbers = [2,4,6,8]
+
+const cuadradoss = numbers.map(numero => numero ** 2)
+console.log(cuadradoss)
+
+
+const incluyes = cuadrados.includes(16)
+console.log(incluyes)
