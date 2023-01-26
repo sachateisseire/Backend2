121192658eda992eff26c8cde2550f7ebddd0403
sachateisseire <sit.teisseire@gmail.com>
2023-01-26 00:59:40 -0300

    Express 26-1

--- nuevoArchivo.js
+++ nuevoArchivo.js
@@ -1,11 +1,47 @@
 import fs from 'fs'
+import express from "express";
+
+const app = express()
+const port = 8000
+
+app.use(express.urlencoded({extended:true}))
+
+app.listen(port, ()=>{
+    console.log(`Server running at port ${port}`)
+})
+
+app.get('/', (req, res)=>{
+    res.send('Para examinar nuestro catálogo, utilice /products para verlo completo, o /products/id para ver alguno en especial')
+})
+
+
+app.get('/products', async (req, res) => {
+    const limit = req.query.limit;
+    let products = await fs.promises.readFile('./products.json');
+    products = JSON.parse(products);
+    if (limit) {
+        products = products.slice(0, limit);
+    }
+    res.json({ products });
+});
+
+app.get('/products/:pid', async (req, res) => {
+    const pid = req.params.pid;
+    const productManager = new ProductManager('./products.json');
+    const product = productManager.getProductById(Number(pid));
+    if (!product) {
+    res.send('Producto no encontrado');
+    }
+    res.json({ product });
+    });
+
 
 class ProductManager {
 
     constructor(path) {
         this.path = path;
         this.products = JSON.parse(fs.readFileSync('./products.json')) || [];
-        this.proximoId = 1;
+        this.proximoId = this.products.length ? Math.max(...this.products.map(p => p.id)) + 1 : 1;
     }
 
     addProduct(product) {
@@ -20,7 +56,8 @@ class ProductManager {
             return;
         }
 
-        product.id = this.proximoId++;
+        product.id = this.proximoId;
+        this.proximoId++;
         this.products.push(product);
         this.save();
     }
@@ -69,15 +106,4 @@ class ProductManager {
     }
 }
 
-const productManager = new ProductManager();
-
-productManager.addProduct({
-  title: 'Trigo',
-  description: 'Descripción del trigo',
-  price: 500,
-  thumbnail: 'http://',
-  code: 'codigo1',
-  stock: 2200
-});
- 
 
